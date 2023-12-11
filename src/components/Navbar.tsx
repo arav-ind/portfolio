157443483aa180de32530b39f5f20a2e72ee1e72
Aravind D <aravindrndv8@gmail.com>
2023-12-11 20:02:00 +0530

    Added responsive menu with animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,16 @@
 import Link from 'next/link'
+import HamMenu from './HamMenu'
 
 export default function Navbar() {
     return (
-      <nav className='flex justify-between h-16 items-center sm:px-32 px-12 font-semibold text-lg'>
+      <nav className='flex relative justify-between h-16 items-center sm:px-32 px-12 font-semibold text-lg z-10'>
         <Link className='flex whitespace-nowrap mr-20' href='/'>Aravind D</Link>
-        <div className='sm:flex hidden md:gap-36 gap-20'>
+        <div className='md:flex hidden md:gap-36 gap-20'>
             <Link href='/?view=works' className=''>Works</Link>
             <Link href='/?view=about'>About</Link>
             <Link href='/?view=extras'>Extras</Link>
-            {/* <div className='flex'>Works</div>
-            <div className='flex'>About</div>
-            <div className='flex'>Extras</div> */}
         </div>
+        <HamMenu />
       </nav>
     )
   }
