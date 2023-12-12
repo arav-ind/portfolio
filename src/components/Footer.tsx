6f882af3b6acfdf9e809df5e9514e48ceb084dfa
Aravind D <aravindrndv8@gmail.com>
2023-12-12 19:10:44 +0530

    Added temporary bg color changer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,12 +7,12 @@ export default function Footer() {
   return (
     <div className='flex flex-col sm:px-32 px-12 flex-wrap gap-4'>
         <h2 className='font-semibold text-xl'>Contact</h2>
-        <div className='flex sm:flex-row flex-col sm:gap-10 gap-5 mb-10 whitespace-nowrap text-md'>
-            <div className='flex gap-2'>
+        <div className='flex sm:flex-row flex-col sm:gap-10 gap-5 mb-6 whitespace-nowrap text-md'>
+            <div className='flex gap-1'>
                 <Image src={mail} alt='mail'/>
                 <span>aravindrndv8@gmail.com</span>
             </div>
-            <div className='flex gap-2'>
+            <div className='flex gap-1'>
                 <Image src={linkedin} alt='mail'/>
                 <Link href='https://www.linkedin.com/in/arav-ind/' target='_blank'>LinkedIn</Link>
             </div>
