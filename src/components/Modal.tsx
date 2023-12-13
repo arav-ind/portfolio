44e7a4dec25cdc1261fb39d3d7aec6fb623770d8
Aravind D <aravindrndv8@gmail.com>
2023-12-13 21:34:31 +0530

    Modified modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -16,60 +16,62 @@ export default function Modal() {
 
     return (
         <div 
-            className={`fixed top-0 w-full h-full z-50 modal-bg overflow-y-scroll transform transition-transform ease-in-out duration-300
+            className={`fixed top-0 w-full h-full z-50 bg-transparent transform transition-transform ease-in-out duration-300
             ${showModal ? 'show-modal' : 'hide-modal'}`}
         >
-            <Image className='absolute top-2 right-2 z-50' onClick={hideModal} src={close} alt='close' />
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
-            <div className='relative mt-4'>This is a modal</div>
+            <Image className='absolute w-6 h-6 top-2 right-2 z-50' onClick={hideModal} src={close} alt='close' />
+            <div className='relative mt-9 w-full h-full bg-white overflow-y-scroll'>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+                <div className='relative mt-4'>This is a modal</div>
+            </div>
         </div>
     )
   }
