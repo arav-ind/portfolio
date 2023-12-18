6ee534364439c497fdc3557e357c0b7cf094fa0a
Aravind D <aravindrndv8@gmail.com>
2023-12-18 13:29:35 +0530

    Testing various routing because of hard reload on prod

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import HamMenu from './HamMenu'
 import { NAVBAR } from '@/constants/constants'
@@ -6,19 +8,26 @@ import work from '@/icons/work.svg'
 import about from '@/icons/about.svg'
 import extras from '@/icons/extras.svg'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 export default function Navbar() {
+  const router = useRouter()
+
+  const handleClick = () => {
+    router.push('/?view=works')
+  }
+
     return (
       <nav className='flex relative md:justify-between justify-center h-16 items-center md:px-32 px-0 font-semibold text-lg z-10'>
         <Link shallow className='flex whitespace-nowrap mr-20' href='/'>
           <span className='absolute lg:hidden flex'><Image src={person} alt='logo'/></span>
           <span className='lg:flex hidden'>{NAVBAR.NAME}</span>
         </Link>
-        <div className='flex md:gap-36 gap-20'>
-            <Link shallow href='/?view=works' className='flex'>
+        <div className='flex md:gap-36 gap-20 cursor-pointer'>
+            <div onClick={handleClick} className='flex'>
               <span className='absolute lg:hidden flex'><Image src={work} alt='logo'/></span>
               <span className='lg:flex hidden'>{NAVBAR.ITEM1}</span>
-            </Link>
+            </div>
             <Link shallow href='/?view=about' className='flex'>
               <span className='absolute lg:hidden flex'><Image src={about} alt='logo'/></span>
               <span className='lg:flex hidden'>{NAVBAR.ITEM2}</span>
