0f1433d3211321a15bf4a09641828a48c4c73779
Aravind D <aravindrndv8@gmail.com>
2023-12-18 16:17:09 +0530

    Changed link to div for link reload bug

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Link from 'next/link'
+// import div from 'next/div'
 import HamMenu from './HamMenu'
 import { NAVBAR } from '@/constants/constants'
 import person from '@/icons/person.svg'
@@ -13,29 +13,25 @@ import { useRouter } from 'next/navigation'
 export default function Navbar() {
   const router = useRouter()
 
-  const handleClick = () => {
-    router.push('/?view=works')
-  }
-
     return (
       <nav className='flex relative md:justify-between justify-center h-16 items-center md:px-32 px-0 font-semibold text-lg z-10'>
-        <Link shallow className='flex whitespace-nowrap mr-20' href='/'>
+        <div className='flex whitespace-nowrap mr-20 cursor-pointer' onClick={() => router.push('/')}>
           <span className='absolute lg:hidden flex'><Image src={person} alt='logo'/></span>
           <span className='lg:flex hidden'>{NAVBAR.NAME}</span>
-        </Link>
+        </div>
         <div className='flex md:gap-36 gap-20 cursor-pointer'>
-            <div onClick={handleClick} className='flex'>
+            <div onClick={() => router.push('/?view=works')} className='flex'>
               <span className='absolute lg:hidden flex'><Image src={work} alt='logo'/></span>
               <span className='lg:flex hidden'>{NAVBAR.ITEM1}</span>
             </div>
-            <Link shallow href='/?view=about' className='flex'>
-              <span className='absolute lg:hidden flex'><Image src={about} alt='logo'/></span>
+            <div onClick={() => router.push('/?view=about')} className='flex'>
+              <span className='absolute lg:hidden flex cursor-pointer'><Image src={about} alt='logo'/></span>
               <span className='lg:flex hidden'>{NAVBAR.ITEM2}</span>
-            </Link>
-            <Link shallow href='/?view=extras' className='flex'>
-              <span className='absolute lg:hidden flex'><Image src={extras} alt='logo'/></span>
+            </div>
+            <div onClick={() => router.push('/?view=extras')} className='flex'>
+              <span className='absolute lg:hidden flex cursor-pointer'><Image src={extras} alt='logo'/></span>
               <span className='lg:flex hidden'>{NAVBAR.ITEM3}</span>
-            </Link>
+            </div>
         </div>
         {/* <HamMenu /> */}
       </nav>
